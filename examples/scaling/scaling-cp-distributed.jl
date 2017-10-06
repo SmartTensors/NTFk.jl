ab8e9590126819868a56a719d32b04ecf9d23e3c
monty <velimir.vesselinov@gmail.com>
2017-10-06 15:02:10 -0600

    distributed added but needs work

---
+++ examples/scaling/scaling-cp-distributed.jl
@@ -0,0 +1,22 @@
+import TensorDecompositions
+import dNTF
+import DistributedArrays
+
+srand(1)
+trank = 3
+for m = 1:5
+	tsize = (10 * m, 20 * m, 5 * m)
+	factors_orig = dNTF.rand_candecomp(trank, tsize, lambdas_nonneg=true, factors_nonneg=true)
+	# T = Array{Float64}(tsize)
+	# Td = DistributedArrays.distribute(T)
+	# T = dNTF.composeditributed!(factors_orig) # this fails
+	T = TensorDecompositions.compose(factors_orig)
+	Td = DistributedArrays.distribute(T)
+	tranks = [1, 2, 3, 4, 5]
+	for t in tranks
+		factors_initial_guess = tuple([randn(dim, t) for dim in tsize]...)
+		info("Tensor rank $t tensor size $tsize")
+		@time factors = TensorDecompositions.candecomp(Td, t, factors_initial_guess, compute_error=true, method=:ALS)
+		@time T_est = dNTF.composeditributed(factors)
+	end
+end
--- src/tensordecompositions-memory.jl
+++ src/tensordecompositions-memory.jl
@@ -1,4 +1,5 @@
 import TensorDecompositions
+import DistributedArrays
 
 @generated function composeshared!{T,N}(dest::SharedArray{T,N}, factors::NTuple{N, Matrix{T}}, lambdas::Vector{T})
 	quote
@@ -19,6 +20,31 @@ end
 	end
 end
 
-composeshared(decomp::TensorDecompositions.CANDECOMP) = composeshared(decomp.factors, decomp.lambdas)
+composeshared(decomp::TensorDecompositions.CANDECOMP) = composedistributed(decomp.factors, decomp.lambdas)
 
-composeshared!{T,N}(dest::SharedArray{T,N}, decomp::TensorDecompositions.CANDECOMP{T,N}) = composeshared!(dest, decomp.factors, decomp.lambdas)
+composeshared!{T,N}(dest::SharedArray{T,N}, decomp::TensorDecompositions.CANDECOMP{T,N}) = composedistributed!(dest, decomp.factors, decomp.lambdas)
+
+@generated function composedistributed!{T,N}(dest::DistributedArrays.DArray{T,N,Array{T,N}}, factors::NTuple{N, Matrix{T}}, lambdas::Vector{T})
+	quote
+		@TensorDecompositions.nloops $N i dest begin
+			elm = zero(T)
+			for j in 1:length(lambdas)
+				elm += lambdas[j] * (*(@TensorDecompositions.ntuple($N, k -> factors[k][i_k, j])...))
+			end
+			@show elm
+			@TensorDecompositions.nref($N, dest, i) = elm
+		end
+		dest
+	end
+end
+
+@generated function composedistributed{T,N}(factors::NTuple{N, Matrix{T}}, lambdas::Vector{T})
+	quote
+		composedistributed!(DistributedArrays.DArray{T,N,Array{T,N}}(@TensorDecompositions.ntuple $N i -> size(factors[i], 1)), factors, lambdas)
+	end
+end
+
+composedistributed(decomp::TensorDecompositions.CANDECOMP) = composedistributed(decomp.factors, decomp.lambdas)
+
+composedistributed!{T,N}(dest::DistributedArrays.DArray{T,N,Array{T,N}}, decomp::TensorDecompositions.CANDECOMP{T,N}) = composedistributed!(dest, decomp.factors, decomp.lambdas)
+
