ed12bf0a7b75638765c8e80e2b198b7eec6a8c10
monty <velimir.vesselinov@gmail.com>
2018-01-07 20:16:09 -0700

    more fixes

--- examples/tensordecomposition/runtests-nmf-tucker.jl
+++ examples/tensordecomposition/runtests-nmf-tucker.jl
@@ -18,50 +18,41 @@ T = T_orig
 
 dNTF.analysis(T, [tsize])
 sizes = [(1,1,1), (2,2,2), (2,2,1), (2,1,1), (2,1,2), (1,2,2), (3,2,1), (2,3,1), (3,3,1)]
-cpf, csize, ibest = dNTF.analysis(T, sizes, 10; ini_decomp=nothing, progressbar=true, tol=1e-8, max_iter=1000)
+tsf, csize, ibest = dNTF.analysis(T, sizes, 10; ini_decomp=nothing, progressbar=true, tol=1e-8, max_iter=1000)
 
-if ibest == 2 || ibest == 3 # these should be the best results; otherwise the comparison fails
-	dNTF.plotcmptensor(T_orig, T_esta[ibest], 3; progressbar=false)
-	@show cor(W[:,1], tucker_spnn[ibest].factors[1][:,1])
-	@show cor(W[:,2], tucker_spnn[ibest].factors[1][:,2])
-	@show cor(H[1,:], tucker_spnn[ibest].factors[2][:,1])
-	@show cor(H[2,:], tucker_spnn[ibest].factors[2][:,2])
-	@show cor(Q, tucker_spnn[ibest].factors[3][:,1])
-else
-	warn("something is not correct; the best results should be #3 (or #2)")
+if ibest != 3
+	warn("Something might be wrong but most probably is not a big deal; the best results should be #3")
+	info("Estimated core size = $csize; best result is #$ibest; the correct core size should be (2,2,1)")
 end
 
-if ibest != 3 # theoretically this should be the best result!!!
-	ibest = 3
-	dNTF.plotcmptensor(T_orig, T_esta[ibest], 3; progressbar=false)
-	@show cor(W[:,1], tucker_spnn[ibest].factors[1][:,1])
-	@show cor(W[:,2], tucker_spnn[ibest].factors[1][:,2])
-	@show cor(H[1,:], tucker_spnn[ibest].factors[2][:,1])
-	@show cor(H[2,:], tucker_spnn[ibest].factors[2][:,2])
-	@show cor(Q, tucker_spnn[ibest].factors[3][:,1])
-	p = Gadfly.plot(
-		Gadfly.Guide.title("Signal 1"),
-		Gadfly.layer(x=1:length(W[:,1]), y=W[:,1], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
-		Gadfly.layer(x=1:length(W[:,1]), y=tucker_spnn[ibest].factors[1][:,1], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
-	display(p); println()
-	p = Gadfly.plot(
-		Gadfly.Guide.title("Signal 2"),
-		Gadfly.layer(x=1:length(W[:,2]), y=W[:,2], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
-		Gadfly.layer(x=1:length(W[:,2]), y=tucker_spnn[ibest].factors[1][:,2], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
-	display(p); println()
-	p = Gadfly.plot(
-		Gadfly.Guide.title("Mixer 1"),
-		Gadfly.layer(x=1:length(H[1,:]), y=H[1,:], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
-		Gadfly.layer(x=1:length(H[1,:]), y=tucker_spnn[ibest].factors[2][:,1], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
-	display(p); println()
-	p = Gadfly.plot(
-		Gadfly.Guide.title("Mixer 2"),
-		Gadfly.layer(x=1:length(H[2,:]), y=H[2,:], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
-		Gadfly.layer(x=1:length(H[2,:]), y=tucker_spnn[ibest].factors[2][:,2], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
-	display(p); println()
-	p = Gadfly.plot(
-		Gadfly.Guide.title("Time change"),
-		Gadfly.layer(x=1:length(Q), y=Q, Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
-		Gadfly.layer(x=1:length(Q), y=tucker_spnn[ibest].factors[3][:,1], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
-	display(p); println()
-end
+dNTF.plotcmptensor(T_orig, T_esta[ibest], 3; progressbar=false)
+@show cor(W[:,1], tsf[ibest].factors[1][:,1])
+@show cor(W[:,2], tsf[ibest].factors[1][:,2])
+@show cor(H[1,:], tsf[ibest].factors[2][:,1])
+@show cor(H[2,:], tsf[ibest].factors[2][:,2])
+@show cor(Q, tsf[ibest].factors[3][:,1])
+p = Gadfly.plot(
+	Gadfly.Guide.title("Signal 1"),
+	Gadfly.layer(x=1:length(W[:,1]), y=W[:,1], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
+	Gadfly.layer(x=1:length(W[:,1]), y=tsf[ibest].factors[1][:,1], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
+display(p); println()
+p = Gadfly.plot(
+	Gadfly.Guide.title("Signal 2"),
+	Gadfly.layer(x=1:length(W[:,2]), y=W[:,2], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
+	Gadfly.layer(x=1:length(W[:,2]), y=tsf[ibest].factors[1][:,2], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
+display(p); println()
+p = Gadfly.plot(
+	Gadfly.Guide.title("Mixer 1"),
+	Gadfly.layer(x=1:length(H[1,:]), y=H[1,:], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
+	Gadfly.layer(x=1:length(H[1,:]), y=tsf[ibest].factors[2][:,1], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
+display(p); println()
+p = Gadfly.plot(
+	Gadfly.Guide.title("Mixer 2"),
+	Gadfly.layer(x=1:length(H[2,:]), y=H[2,:], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
+	Gadfly.layer(x=1:length(H[2,:]), y=tsf[ibest].factors[2][:,2], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
+display(p); println()
+p = Gadfly.plot(
+	Gadfly.Guide.title("Time change"),
+	Gadfly.layer(x=1:length(Q), y=Q, Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "blue"))),
+	Gadfly.layer(x=1:length(Q), y=tsf[ibest].factors[3][:,1], Gadfly.Geom.line, Gadfly.Theme(default_color=parse(Colors.Colorant, "red"))))
+display(p); println()
--- examples/tensordecomposition/runtests-signal-tucker.jl
+++ examples/tensordecomposition/runtests-signal-tucker.jl
@@ -2,13 +2,13 @@ import dNTF
 
 tsize = (2, 10, 7)
 T_orig = Array{Float64}(tsize)
-T_orig[:,:,1] = [zeros(2,10)]
+T_orig[:,:,1] = zeros(2,10)
 T_orig[:,:,2] = [ones(2,2) zeros(2,8)]
 T_orig[:,:,3] = [zeros(2,2) ones(2,2) zeros(2,6)]
 T_orig[:,:,4] = [zeros(2,4) ones(2,2) zeros(2,4)]
 T_orig[:,:,5] = [zeros(2,6) ones(2,2) zeros(2,2)]
 T_orig[:,:,6] = [zeros(2,8) ones(2,2)]
-T_orig[:,:,7] = [zeros(2,10)]
+T_orig[:,:,7] = zeros(2,10)
 T_orig .*= 100
 
 # T = dNTF.add_noise(T_orig, 0.6, true)
@@ -16,5 +16,11 @@ T = T_orig
 
 sizes = [(1,4,4), (1,4,4), (1,4,4), (1,4,4), (1,5,4), (1,5,4), (1,5,4), (1,5,4), (1,5,4)]
 # sizes = [(1,2,2), (1,2,2), (1,2,2), (1,2,2), (1,2,3), (1,2,3), (1,2,3), (1,2,3), (1,2,3)]
-t, c, ibest = dNTF.analysis(T, [tsize]; progressbar=true)
+t, c, ibest = dNTF.analysis(T, [tsize]; progressbar=true, tol=1e-16, max_iter=50000)
 dNTF.plotcmptensor(T, t[ibest], 3; progressbar=false)
+dNTF.plotmatrix(t[ibest].factors[1])
+dNTF.plotmatrix(t[ibest].factors[2])
+dNTF.plotmatrix(t[ibest].factors[3])
+dNTF.plotmatrix(t[ibest].core[1,:,:])
+
+map!(x -> x / 100, t[ibest].core, t[ibest].core)
