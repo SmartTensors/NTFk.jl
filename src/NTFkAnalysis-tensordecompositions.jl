678958143514db1435201a4a7a41f8d20c4d35f1
monty <velimir.vesselinov@gmail.com>
2018-07-27 09:16:10 -0400

    atensor mask output added

--- src/NTFkAnalysis-tensordecompositions.jl
+++ src/NTFkAnalysis-tensordecompositions.jl
@@ -360,8 +360,10 @@ function atensor(X::Array)
 	nd = ndims(X)
 	info("Number of dimensions: $nd")
 	tsize = size(X)
+	mask = Vector{Vector{Bool}}(nd)
 	for i = 1:nd
 		info("D$i ($(tsize[i]))")
+		mask[i] = trues(tsize[i])
 		for j = 1:tsize[i]
 			st = ntuple(k->(k == i ? j : Colon()), nd)
 			if nd == 3
@@ -371,9 +373,13 @@ function atensor(X::Array)
 			end
 			z = count(X[st...] .> 0)
 			println("$j : rank $r non-zeros $z")
+			if z == 0
+				mask[i][j] = false
+			end
 			# display(X[st...])
 		end
 	end
+	return mask
 end
 
 function normalizefactors!{T,N}(X::TensorDecompositions.Tucker{T,N})
