195e9dcf156ea1705c50cab22b505d575970a3f4
monty <velimir.vesselinov@gmail.com>
2018-10-07 09:31:32 -0600

    computestats fix

--- src/NTFkHelpers.jl
+++ src/NTFkHelpers.jl
@@ -14,27 +14,28 @@ function minimumnan(X, c...; kw...)
 	minimum(X[.!isnan.(X)], c...; kw...)
 end
 
-function computestats(X, Xe, volumeindex=1:size(Xe,1), wellindex=1:size(Xe,3), timeindex=:, c="")
-	ferr = Array{Float64}(3)
-	wsum1 = Array{Float64}(11)
-	wsum2 = Array{Float64}(11)
-	werr = Array{Float64}(11)
-	merr = Array{Float64}(3)
-	fcor = Array{Float64}(3)
+function computestats(X, Xe, volumeindex=1:size(Xe,1), wellindex=1:size(Xe,3), timeindex=:, c=""; plot=true, quiet=true, wellnames=nothing, xaxis=1:size(Xe,2))
+	ferr = Array{Float64}(length(volumeindex))
+	wsum1 = Array{Float64}(length(wellindex))
+	wsum2 = Array{Float64}(length(wellindex))
+	werr = Array{Float64}(length(wellindex))
+	merr = Array{Float64}(length(volumeindex))
+	fcor = Array{Float64}(length(volumeindex))
 	f = "%-85s"
 	g = "%-.2g"
 	for (i, v) in enumerate(volumeindex)
-		@show NMFk.vecnormnan(Xe[wellindex,timeindex,v] .- X[wellindex,timeindex,v])
 		se = NMFk.sumnan(Xe[wellindex,timeindex,v])
 		s =  NMFk.sumnan(X[wellindex,timeindex,v])
 		# @show se
 		# @show s
 		ferr[i] = (se - s) / s
-		for j in wellindex
-			wsum2[j] = NMFk.sumnan(Xe[j,timeindex,v])
-			wsum1[j] = NMFk.sumnan(X[j,timeindex,v])
+		for (j, w) in enumerate(wellindex)
+			wsum2[j] = NMFk.sumnan(Xe[w,timeindex,v])
+			wsum1[j] = NMFk.sumnan(X[w,timeindex,v])
 			werr[j] = abs.(wsum2[j] - wsum1[j]) / wsum1[j]
 		end
+		# @show se
+		# @show s
 		# @show wsum2
 		# @show wsum1
 		# @show werr
@@ -42,6 +43,7 @@ function computestats(X, Xe, volumeindex=1:size(Xe,1), wellindex=1:size(Xe,3), t
 		fcor[i] = cor(vec(wsum1), vec(wsum2))
 	end
 	info("$(NMFk.sprintf(f, c)): $(NMFk.vecnormnan(X[wellindex,timeindex,volumeindex] .- Xe[wellindex,timeindex,volumeindex])) [Error: $(NMFk.sprintf(g, ferr[1])) $(NMFk.sprintf(g, ferr[2])) $(NMFk.sprintf(g, ferr[3]))] [Max error: $(NMFk.sprintf(g, merr[1])) $(NMFk.sprintf(g, merr[2])) $(NMFk.sprintf(g, merr[3]))] [Pearson: $(NMFk.sprintf(g, fcor[1])) $(NMFk.sprintf(g, fcor[2])) $(NMFk.sprintf(g, fcor[3]))]")
+	plot && NTFk.plot2d(X, Xe; quiet=quiet, figuredir="results-12-18", keyword=lowercase(replace(replace(c, " ", "_"), "/", "_")), titletext=c, wellnames=wellnames, dimname="Well", xaxis=xaxis, xmin=Dates.Date(2015,12,15), xmax=Dates.Date(2017,6,10), linewidth=1.5Gadfly.pt)
 end
 
 function flatten(X::Array{T,N}, mask::BitArray{M}) where {T,N,M}
