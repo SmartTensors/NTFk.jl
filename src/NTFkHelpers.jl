3882836fd201b60e6e4a44253793894b4919e64a
monty <velimir.vesselinov@gmail.com>
2018-10-07 14:54:36 -0600

    minor

--- src/NTFkHelpers.jl
+++ src/NTFkHelpers.jl
@@ -15,14 +15,19 @@ function minimumnan(X, c...; kw...)
 end
 
 function computestats(X, Xe, volumeindex=1:size(Xe,1), wellindex=1:size(Xe,3), timeindex=:, c=""; plot=true, quiet=true, wellnames=nothing, xaxis=1:size(Xe,2))
+	m = "%-85s"
+	if Xe == nothing
+		info("$(NMFk.sprintf(m, c)): fails")
+		return
+	end
 	ferr = Array{Float64}(length(volumeindex))
 	wsum1 = Array{Float64}(length(wellindex))
 	wsum2 = Array{Float64}(length(wellindex))
 	werr = Array{Float64}(length(wellindex))
 	merr = Array{Float64}(length(volumeindex))
 	fcor = Array{Float64}(length(volumeindex))
-	f = "%-85s"
-	g = "%-.2g"
+	g = "%.2g"
+	f = "%.2f"
 	for (i, v) in enumerate(volumeindex)
 		se = NMFk.sumnan(Xe[wellindex,timeindex,v])
 		s =  NMFk.sumnan(X[wellindex,timeindex,v])
@@ -42,7 +47,7 @@ function computestats(X, Xe, volumeindex=1:size(Xe,1), wellindex=1:size(Xe,3), t
 		merr[i] = maximum(werr)
 		fcor[i] = cor(vec(wsum1), vec(wsum2))
 	end
-	info("$(NMFk.sprintf(f, c)): $(NMFk.vecnormnan(X[wellindex,timeindex,volumeindex] .- Xe[wellindex,timeindex,volumeindex])) [Error: $(NMFk.sprintf(g, ferr[1])) $(NMFk.sprintf(g, ferr[2])) $(NMFk.sprintf(g, ferr[3]))] [Max error: $(NMFk.sprintf(g, merr[1])) $(NMFk.sprintf(g, merr[2])) $(NMFk.sprintf(g, merr[3]))] [Pearson: $(NMFk.sprintf(g, fcor[1])) $(NMFk.sprintf(g, fcor[2])) $(NMFk.sprintf(g, fcor[3]))]")
+	info("$(NMFk.sprintf(m, c)): $(NMFk.sprintf(f, NMFk.vecnormnan(X[wellindex,timeindex,volumeindex] .- Xe[wellindex,timeindex,volumeindex]))) [Error: $(NMFk.sprintf(g, ferr[1])) $(NMFk.sprintf(g, ferr[2])) $(NMFk.sprintf(g, ferr[3]))] [Max error: $(NMFk.sprintf(g, merr[1])) $(NMFk.sprintf(g, merr[2])) $(NMFk.sprintf(g, merr[3]))] [Pearson: $(NMFk.sprintf(g, fcor[1])) $(NMFk.sprintf(g, fcor[2])) $(NMFk.sprintf(g, fcor[3]))]")
 	plot && NTFk.plot2d(X, Xe; quiet=quiet, figuredir="results-12-18", keyword=lowercase(replace(replace(c, " ", "_"), "/", "_")), titletext=c, wellnames=wellnames, dimname="Well", xaxis=xaxis, xmin=Dates.Date(2015,12,15), xmax=Dates.Date(2017,6,10), linewidth=1.5Gadfly.pt)
 end
 
