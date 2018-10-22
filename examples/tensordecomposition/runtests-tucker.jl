3f753a80821837b9e885a3d87832a15b6ff3a28b
monty <velimir.vesselinov@gmail.com>
2018-10-22 09:05:42 -0600

    tensorly added

--- examples/tensordecomposition/runtests-tucker.jl
+++ examples/tensordecomposition/runtests-tucker.jl
@@ -12,3 +12,12 @@ T = T_orig
 
 sizes = [csize, (1,3,4), (3,3,4), (2,2,4), (2,4,4), (2,3,3), (2,3,5)]
 NTFk.analysis(T, sizes, 10; progressbar=true, tol=1e-6, max_iter=1000)
+
+srand(1)
+csize = (100, 10, 10)
+tsize = (1000, 100, 100)
+tucker_orig = NTFk.rand_tucker(csize, tsize, factors_nonneg=true, core_nonneg=true)
+T = TensorDecompositions.compose(tucker_orig)
+
+sizes = [csize]
+NTFk.analysis(T, sizes, 10; progressbar=false, tol=1e-12, max_iter=10)
---
+++ src/NTFkAnalysis-tensorly.jl
@@ -0,0 +1,63 @@
+import PyCall
+import NTFk
+
+@PyCall.pyimport tensorly as tl
+@PyCall.pyimport tensorly.decomposition as td
+
+srand(1)
+csize = (10,10,10)
+tsize = (1000, 1000, 1000)
+tucker_orig = NTFk.rand_tucker(csize, tsize, factors_nonneg=true, core_nonneg=true)
+T_orig = TensorDecompositions.compose(tucker_orig);
+
+info("numpy")
+tl.set_backend("numpy")
+@time T_orig_tl = tl.backend[:tensor](T_orig);
+@time td.non_negative_tucker(T_orig_tl, rank=csize, n_iter_max=2);
+
+info("mxnet")
+tl.set_backend("mxnet")
+@time T_orig_tl = tl.backend[:tensor](T_orig);
+@time td.non_negative_tucker(T_orig_tl, rank=csize, n_iter_max=2);
+
+info("pytorch")
+tl.set_backend("pytorch")
+@time T_orig_tl = tl.backend[:tensor](T_orig);
+@time td.non_negative_tucker(T_orig_tl, rank=csize, n_iter_max=2);
+
+info("tensorflow")
+tl.set_backend("tensorflow");
+@time T_orig_tl = tl.backend[:tensor](T_orig);
+@time td.non_negative_tucker(T_orig_tl, rank=csize, n_iter_max=2);
+
+info("numpy")
+tl.set_backend("numpy")
+@time td.non_negative_tucker(tl.backend[:tensor](T_orig), rank=csize, n_iter_max=2);
+
+info("mxnet")
+tl.set_backend("mxnet")
+@time td.non_negative_tucker(tl.backend[:tensor](T_orig), rank=csize, n_iter_max=2);
+
+info("pytorch")
+tl.set_backend("pytorch")
+@time td.non_negative_tucker(tl.backend[:tensor](T_orig), rank=csize, n_iter_max=2);
+
+info("tensorflow")
+tl.set_backend("tensorflow");
+@time td.non_negative_tucker(tl.backend[:tensor](T_orig), rank=csize, n_iter_max=2);
+
+info("numpy")
+tl.set_backend("numpy")
+@time @pycall(td.non_negative_tucker(tl.backend[:tensor](T_orig), rank=csize, n_iter_max=2));
+
+info("mxnet")
+tl.set_backend("mxnet")
+@time @pycall(td.non_negative_tucker(tl.backend[:tensor](T_orig), rank=csize, n_iter_max=2));
+
+info("pytorch")
+tl.set_backend("pytorch")
+@time @pycall(td.non_negative_tucker(tl.backend[:tensor](T_orig), rank=csize, n_iter_max=2));
+
+info("tensorflow")
+tl.set_backend("tensorflow");
+@time @pycall(td.non_negative_tucker(tl.backend[:tensor](T_orig), rank=csize, n_iter_max=2));
