933154918261da31c79cd878209b26f31ff24db1
monty <velimir.vesselinov@gmail.com>
2018-12-10 09:52:57 -0700

    TD removed

--- src/NTFkAnalysis.jl
+++ src/NTFkAnalysis.jl
@@ -1,20 +1,11 @@
-import CanDecomp
-import StaticArrays
-import TensorDecompositions
-
-function janalysis(T::Array, crank::Number; seed::Number=1)
-	sizes = size(T)
-	ndimension = length(sizes)
-	factors = Vector{Matrix{Float64}}(ndimension)
-	for i = 1:ndimension
-		factors[i] = zeros(crank, sizes[i]) #TODO CanDecomp.candecomp work with transposed factor matrices
+function analysis(X::AbstractArray{T,N}, dsizes::Vector{Int64}, dim, nTF; kw...) where {T,N}
+	csize = collect(size(X))
+	ndimensons = length(csize)
+	sizes = Vector{Tuple}(0)
+	for i in dsizes
+		nt = ntuple(k->(k == dim ? i : csize[k]), ndimensons)
+		push!(sizes, nt)
 	end
-	CanDecomp.candecomp!(StaticArrays.SVector(factors...), deepcopy(T); regularization=0., max_cd_iters=1, tol=0.1, max_iter=3)
-	TT = TensorDecompositions.CANDECOMP((permutedims(factors)...,), ones(crank)) # transpose factor matrices
-	return TT
-end
-
-function janalysis(T::Array, crank::Vector; seed::Number=1)
-	# TT = TensorDecompositions.Tucker((R["u"][1:end]...), R["core"]["data"])
-	# return TT
+	info("Sizes: $(sizes)")
+	analysis(X, sizes, nTF; kw...)
 end
