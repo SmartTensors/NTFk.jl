9f12ec1f65f9f11bd68496cbfb23ec0e3a097fa6
monty <velimir.vesselinov@gmail.com>
2019-01-31 18:01:00 -0700

    NTFkLoadTensorDecompositions added

--- src/NTFkLoadTensorDecompositions.jl
+++ src/NTFkLoadTensorDecompositions.jl
@@ -3,5 +3,14 @@ import TensorDecompositions
 
 function loadtucker(f::String, arg...; kw...)
 	core, factors, props = LoadTensorDecompositions.loadtucker(f, arg...; kw...)
-	return TensorDecompositions.Tucker(factors, core)
+	if typeof(core) <: AbstractVector
+		n = length(core)
+		t = Vector{Any}(undef, 0)
+		for i = 1:n
+			push!(t, TensorDecompositions.Tucker(factors[i], core[i]))
+		end
+		return t
+	else
+		return TensorDecompositions.Tucker(factors, core)
+	end
 end
