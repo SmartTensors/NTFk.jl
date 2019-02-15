eadc2166f2571b1e895ebbf072e2f379d8cf5ecf
monty <velimir.vesselinov@gmail.com>
2019-02-15 10:34:24 -0700

    load fix

--- src/NTFkLoadTensorDecompositions.jl
+++ src/NTFkLoadTensorDecompositions.jl
@@ -2,15 +2,22 @@ import LoadTensorDecompositions
 import TensorDecompositions
 
 function loadtucker(f::String, arg...; kw...)
-	core, factors, props = LoadTensorDecompositions.loadtucker(f, arg...; kw...)
-	if typeof(core) <: AbstractVector
-		n = length(core)
-		t = Vector{Any}(undef, 0)
-		for i = 1:n
-			push!(t, TensorDecompositions.Tucker(factors[i], core[i]))
+	if isfile(f)
+		ans = LoadTensorDecompositions.loadtucker(f, arg...; kw...)
+		if ans != nothing
+			core, factors, props = ans
+			if typeof(core) <: AbstractVector
+				n = length(core)
+				t = Vector{Any}(undef, 0)
+				for i = 1:n
+					push!(t, TensorDecompositions.Tucker(factors[i], core[i]))
+				end
+				return t
+			else
+				return TensorDecompositions.Tucker(factors, core)
+			end
 		end
-		return t
 	else
-		return TensorDecompositions.Tucker(factors, core)
+		@warn("File $f does not exist!")
 	end
 end
