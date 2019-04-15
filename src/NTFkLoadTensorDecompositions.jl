72c28461783b013e2d1a304b473962d68a6fbfeb
monty <velimir.vesselinov@gmail.com>
2019-04-15 17:41:17 -0600

    localloadtucker added

--- src/NTFkLoadTensorDecompositions.jl
+++ src/NTFkLoadTensorDecompositions.jl
@@ -1,8 +1,14 @@
 import LoadTensorDecompositions
 import TensorDecompositions
+import FileIO
+import JLD
+import JLD2
 
 function loadtucker(f::String, arg...; kw...)
-	if isfile(f)
+	if !isfile(f)
+		@warn("File $f does not exist!")
+	end
+	try
 		ans = LoadTensorDecompositions.loadtucker(f, arg...; kw...)
 		if ans != nothing
 			core, factors, props = ans
@@ -16,8 +22,35 @@ function loadtucker(f::String, arg...; kw...)
 			else
 				return TensorDecompositions.Tucker(factors, core)
 			end
+		else
+			@warn("There is a problem reading file $f!")
+			return nothing
 		end
+	catch
+		return localloadtucker(f::String, arg...; kw...)
+	end
+end
+
+function localloadtucker(f::String, arg...; kw...)
+	d = FileIO.load(f)
+	if [keys(d)...][1] == "tucker_vector"
+		di = d["tucker_vector"]
+		n = length(di)
+		t = Vector{Any}(undef, 0)
+		for i = 1:n
+			push!(t, TensorDecompositions.Tucker(di[i].factors, di[i].core))
+		end
+		return t
 	else
-		@warn("File $f does not exist!")
+		di = d["tucker"]
+		return TensorDecompositions.Tucker(di.factors, di.core)
+	end
+end
+
+function savetucker(tucker_spnn, f::String)
+	if typeof(tucker_spnn) <: TensorDecompositions.Tucker
+		FileIO.save(f, "tucker", tucker_spnn)
+	else
+		FileIO.save(f, "tucker_vector", tucker_spnn)
 	end
 end
