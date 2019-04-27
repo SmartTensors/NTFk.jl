7513553b6f93d16681d8d5e4780c0860411b3b8d
monty <velimir.vesselinov@gmail.com>
2019-04-27 09:48:19 -0600

    distributed-benchmark generelized

--- examples/scaling/distributed-benchmark.jl
+++ examples/scaling/distributed-benchmark.jl
@@ -5,37 +5,40 @@ import LinearAlgebra
 import BenchmarkTools
 import DistributedArrays
 
-@info("Set worker BLAS to one thread only")
-@sync for p in Distributed.workers()
-	@async Distributed.remotecall_wait(LinearAlgebra.BLAS.set_num_threads, p , 1)
-end
-
-const suite = BenchmarkTools.BenchmarkGroup()
-suite["Local"] = BenchmarkTools.BenchmarkGroup()
-suite["Distributed"] = BenchmarkTools.BenchmarkGroup()
-
-function benchmark(T=Array, N=10)
-	@BenchmarkTools.benchmarkable A * B setup = (A = $T(rand($N, $N)); B = $T(rand($N, $N)))
-end
-
-for N in (2^i for i = 5:13)
-	suite["Local"][N] = benchmark(Array, N)
-	suite["Distributed"][N] = benchmark(DistributedArrays.distribute, N)
-end
-
 function printresults(results)
 	@info("Local")
-	for (name, trial) in sort(collect(results["Local"]), by=x->time(x[2]))
+	for (name, trial) in sort(collect(results["Local"]), by=x->x[1])
 		t = time(trial) / 1e6
 		println(rpad(name, 25, "."), lpad(string(round(t, digits=2), " ms"), 20, "."))
 	end
 	@info("Distributed")
-	for (name, trial) in sort(collect(results["Distributed"]), by=x->time(x[2]))
+	for (name, trial) in sort(collect(results["Distributed"]), by=x->x[1])
 		t = time(trial) / 1e6
 		println(rpad(name, 25, "."), lpad(string(round(t, digits=2), " ms"), 20, "."))
 	end
 end
 
+function setbenchmark(A=Array, T=Float32, N=10; nsamples=10, nevals=1)
+	@BenchmarkTools.benchmarkable A * B setup=(A = $A(rand($T, $N, $N)); B = $A(rand($T, $N, $N))) samples=nsamples evals=nevals
+end
+
+function setbenchmark(r::Integer=7, T=Float32; nsamples=10, nevals=1)
+	for N in (2^i for i = 5:r)
+		suite["Local"][N] = setbenchmark(Array, T, N; nsamples=nsamples)
+		suite["Distributed"][N] = setbenchmark(DistributedArrays.distribute, T, N; nsamples=nsamples, nevals=nevals)
+	end
+end
+
+# @info("Set worker BLAS to one thread only")
+# @sync for p in Distributed.workers()
+# 	@async Distributed.remotecall_wait(LinearAlgebra.BLAS.set_num_threads, p , 1)
+# end
+
+suite = BenchmarkTools.BenchmarkGroup()
+suite["Local"] = BenchmarkTools.BenchmarkGroup()
+suite["Distributed"] = BenchmarkTools.BenchmarkGroup()
+
+setbenchmark(13, Float16; nsamples=10, nevals=1)
 BenchmarkTools.tune!(suite)
 results = BenchmarkTools.run(suite)
 printresults(results)
--- examples/scaling/scaling-cp-distributed.jl
+++ examples/scaling/scaling-cp-distributed.jl
@@ -11,16 +11,14 @@ trank = 3
 for m = 1:5
 	tsize = (10 * m, 20 * m, 5 * m)
 	factors_orig = NTFk.rand_candecomp(trank, tsize, lambdas_nonneg=true, factors_nonneg=true)
-	# T = Array{Float64}(undef, tsize)
-	# Td = DistributedArrays.distribute(T)
-	# T = NTFk.composedistributed(factors_orig) # this fails
 	T = TensorDecompositions.compose(factors_orig)
-	Td = DistributedArrays.distribute(T)
+	dT = DistributedArrays.distribute(T)
 	tranks = [1, 2, 3, 4, 5]
 	for t in tranks
 		factors_initial_guess = tuple([randn(dim, t) for dim in tsize]...)
 		@info("Tensor rank $t tensor size $tsize")
-		@time factors = TensorDecompositions.candecomp(Td, t, factors_initial_guess, compute_error=true, method=:ALS)
-		@time T_est = NTFk.composedistributed(factors)
+		@time factors = TensorDecompositions.candecomp(dT, t, factors_initial_guess, compute_error=true, method=:ALS)
+		# @time T_est = NTFk.composedistributed(factors)
+		@time T_est = TensorDecompositions.compose(factors)
 	end
 end
