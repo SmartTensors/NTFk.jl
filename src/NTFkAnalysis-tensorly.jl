9b13a9713e76e7453aac3bd161537e8a21d0be86
monty <velimir.vesselinov@gmail.com>
2019-05-26 17:01:15 -0600

    tensorly fixes

--- src/NTFkAnalysis-tensorly.jl
+++ src/NTFkAnalysis-tensorly.jl
@@ -1,19 +1,12 @@
 import PyCall
 
 const tensorly = PyCall.PyNULL()
-const tensorlydecomp = PyCall.PyNULL()
 function __init__()
 	try
 		copy!(tensorly, PyCall.pyimport("tensorly"))
 		@info("TensorLy is available")
 	catch
 		@warn("TensorLy is not available")
-	end
-	try
-		copy!(tensorlydecomp, PyCall.pyimport("tensorly.decomposition"))
-		@info("TensorLy.decomposition is available")
-	catch
-		@warn("TensorLy.decomposition is not available")
 	end
 end
 
@@ -25,7 +18,7 @@ converter = :numpy, :numpy, :asnumpy, (conversion not needed)
 function tlanalysis(X::Array{T,N}, crank::NTuple{N, Int}; seed::Number=1, backend="tensorflow", converter="numpy", functionname::AbstractString="non_negative_tucker", init::String="svd", maxiter::Integer=DMAXITER, tol::Number=1e-4, verbose::Bool=false) where {T, N}
 	tensorly.set_backend(backend)
 	@info("Tensorly backend: $backend")
-	func = eval(:(tensorlydecomp.$functionname))
+	func = eval(:(tensorly.decomposition.$functionname))
 	core, factors = func(tensorly.backend.tensor(X), rank=[crank...], n_iter_max=maxiter, init=init, tol=tol, verbose=verbose);
 	nc = length(factors)
 	f = Vector{Array}(undef, nc)
