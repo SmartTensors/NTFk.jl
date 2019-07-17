9acfdf6fc9e30ad55df9fc81d741be1c5923017b
monty <velimir.vesselinov@gmail.com>
2019-07-17 00:33:52 -0600

    fuctions moved

--- src/NTFkHelp.jl
+++ src/NTFkHelp.jl
@@ -96,3 +96,51 @@ NTFk.functions("get")
 NTFk.functions(NTFk, "get")
 ```
 """ functions
+
+"Checks if package is available"
+function ispkgavailable(modulename::String; quiet::Bool=false)
+	flag=false
+	try
+		Pkg.available(modulename)
+		if typeof(Pkg.installed(modulename)) == Nothing
+			flag=false
+			!quiet && @info("Module $modulename is not available")
+		else
+			flag=true
+		end
+	catch
+		!quiet && @info("Module $modulename is not available")
+	end
+	return flag
+end
+
+"Print error message"
+function printerrormsg(errmsg::Any)
+	Base.showerror(Base.stderr, errmsg)
+	try
+		if in(:msg, fieldnames(errmsg))
+			@warn(strip(errmsg.msg))
+		elseif typeof(errmsg) <: AbstractString
+			@warn(errmsg)
+		end
+	catch
+		@warn(errmsg)
+	end
+end
+
+"Try to import a module"
+macro tryimport(s::Symbol)
+	mname = string(s)
+	importq = string(:(import $s))
+	infostring = string("Module ", s, " is not available")
+	warnstring = string("Module ", s, " cannot be imported")
+	q = quote
+		try
+			eval(Meta.parse($importq))
+		catch errmsg
+			printerrormsg(errmsg)
+			@warn($warnstring)
+		end
+	end
+	return :($(esc(q)))
+end
