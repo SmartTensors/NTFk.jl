04e7434e37680c86b62de386cf8dde903818b435
monty <velimir.vesselinov@gmail.com>
2019-07-19 02:23:08 -0600

    NTFkDistributed

--- examples/scaling/scaling-cp-distributed.jl
+++ examples/scaling/scaling-cp-distributed.jl
@@ -11,7 +11,7 @@ for m = 1:5
 	tsize = (10 * m, 20 * m, 5 * m)
 	factors_orig = NTFk.rand_candecomp(trank, tsize, lambdas_nonneg=true, factors_nonneg=true)
 	T = NTFk.compose(factors_orig)
-	dT = DistributedArrays.distribute(T)
+	dT = TensorDecompositions.distribute(T)
 	tranks = [1, 2, 3, 4, 5]
 	for t in tranks
 		factors_initial_guess = tuple([randn(dim, t) for dim in tsize]...)
--- src/NTFkDistributed.jl
+++ src/NTFkDistributed.jl
@@ -6,9 +6,10 @@ Distribute
 $(DocumentFunction.documentfunction(distribute))
 """
 function distribute(X::AbstractArray{T,N}) where {T,N}
-	dX = DistributedArrays.distribute(X)
 	sz = size(X)
 	@info "Dimension 1: Distributing array with size $(sz)"
+	dX = DistributedArrays.distribute(X)
+	global dX1 = dX
 	for i = 2:N
 		a = [i for i = 1:N]
 		k = a .!= i
