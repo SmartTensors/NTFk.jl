7d48b496ee7b8b5825158d6f85baec33d5c2942f
monty <velimir.vesselinov@gmail.com>
2019-07-20 01:03:04 -0600

    minor

--- examples/scaling/scaling-tucker-distributed.jl
+++ examples/scaling/scaling-tucker-distributed.jl
@@ -7,7 +7,7 @@ import DistributedArrays
 import Random
 
 Random.seed!(1)
-trank = 3
+trank = (2, 3, 4)
 for m = 1:5
 	tsize = (5 * m, 10 * m, 20 * m)
 	tucker_orig = NTFk.rand_tucker(trank, tsize; core_nonneg=true, factors_nonneg=true)
