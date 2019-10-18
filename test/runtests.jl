58872f2c5d628c3864e9bf73929d35d4dc52305c
monty <velimir.vesselinov@gmail.com>
2019-10-18 11:43:03 -0600

    test fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,7 +22,7 @@ import LinearAlgebra
 	tucker_est, csize_est, ibest = NTFk.analysis(T_orig, [csize], 1; seed=1, eigmethod=[false,false,false], progressbar=false, tol=1e-4, maxiter=100, lambda=0.)
 	T_est = TensorDecompositions.compose(tucker_est[ibest])
 	@Test.test csize == csize_est
-	display(LinearAlgebra.norm(T_orig .- T_est))
+	@info "Error $display(LinearAlgebra.norm(T_orig .- T_est))"
 	@Test.test isapprox(LinearAlgebra.norm(T_orig .- T_est), 1.0104725439065432, atol=1e-3)
 end
 
@@ -34,8 +34,8 @@ end
 	T_orig = TensorDecompositions.compose(tucker_orig)
 	cp_est, csize, ibest = NTFk.analysis(T_orig, [maximum(csize)], 1; seed=1, eigmethod=[false,false,false], progressbar=false, tol=1e-12, maxiter=100, lambda=0.)
 	T_est = TensorDecompositions.compose(cp_est[ibest])
-	display(LinearAlgebra.norm(T_orig .- T_est))
-	@Test.test isapprox(LinearAlgebra.norm(T_orig .- T_est), 0.047453937969484744, atol=1e-3)
+	@info "Error $(LinearAlgebra.norm(T_orig .- T_est))"
+	@Test.test isapprox(LinearAlgebra.norm(T_orig .- T_est), 0.047453937969484744; atol=1e-3)
 end
 
 @Test.testset "NTFk TensorDecompositions Tucker sparse analysis" begin
@@ -48,9 +48,9 @@ end
 	nans = .!isnan.(T_orig)
 	tucker_est, csize_est, ibest = NTFk.analysis(T_orig, [csize], 1; seed=1, eigmethod=[false,false,false], progressbar=false, tol=1e-4, maxiter=100, lambda=0.)
 	T_est = TensorDecompositions.compose(tucker_est[ibest])
-	display(LinearAlgebra.norm(T_orig[nans] .- T_est[nans]))
+	@info "Error $(LinearAlgebra.norm(T_orig[nans] .- T_est[nans]))"
 	@Test.test csize == csize_est
-	@Test.test isapprox(LinearAlgebra.norm(T_orig[.!nans] .- T_est[.!nans]), 1.0332311547429105, atol=1e-2)
+	@Test.test isapprox(LinearAlgebra.norm(T_orig[nans] .- T_est[nans]), 1.0332311547429096; atol=1e-2)
 end
 
 if NTFk.tensorly == PyCall.PyNULL()
@@ -70,8 +70,8 @@ else
 			tucker_est, csize_est, ibest = a
 			T_est = TensorDecompositions.compose(tucker_est[ibest])
 			@Test.test csize == csize_est
-			display(LinearAlgebra.norm(T_orig .- T_est))
-			@Test.test isapprox(LinearAlgebra.norm(T_orig .- T_est), 0.913, atol=1e-3)
+			@info "Error $(LinearAlgebra.norm(T_orig .- T_est))"
+			@Test.test isapprox(LinearAlgebra.norm(T_orig .- T_est), 0.913; atol=1e-3)
 		end
 	end
 end
