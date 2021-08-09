8b142ffed97776d96b44bd49ffb6330141193cb6
monty <velimir.vesselinov@gmail.com>
2021-08-09 14:54:56 -0600

    fix

--- src/NTFkNotebooks.jl
+++ src/NTFkNotebooks.jl
@@ -1,3 +1,5 @@
+import Mads
+
 notebook(a...; k...) = Mads.notebook(a...; dir=NTFk.dir, k...)
 notebooks(a...; k...) = Mads.notebooks(a...; dir=NTFk.dir, k...)
 notebookscript(a...; k...) = Mads.notebookscript(a...; dir=NTFk.dir, k...)
