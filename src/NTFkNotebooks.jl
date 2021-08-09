43d1e617d2f17b4857e67705ef2f6b1f2decb71f
monty <velimir.vesselinov@gmail.com>
2021-08-09 09:26:29 -0600

    notebooks

--- src/NTFkNotebooks.jl
+++ src/NTFkNotebooks.jl
@@ -1,3 +1,4 @@
-notebook(a..., k...) = Mads.notebook(a...; dir=NTFk.dir, k...)
-notebooks(a..., k...) = Mads.notebooks(a...; dir=NTFk.dir, k...)
-process_notebook(a..., k...) = Mads.process_notebook(a...; dir=NTFk.dir, k...)
+notebook(a...; k...) = Mads.notebook(a...; dir=NTFk.dir, k...)
+notebooks(a...; k...) = Mads.notebooks(a...; dir=NTFk.dir, k...)
+notebookscript(a...; k...) = Mads.notebookscript(a...; dir=NTFk.dir, k...)
+process_notebook(a...; k...) = Mads.process_notebook(a...; dir=NTFk.dir, k...)
