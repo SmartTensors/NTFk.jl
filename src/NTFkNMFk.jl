ace481ce48fe1dd8403cbd9e94f76fbc9a3c2b1c
Velimir (monty) Vesselinov <velimir.vesselinov@gmail.com>
2024-07-17 08:03:21 -0600

    NMFk.tensorfactorization

--- src/NTFkNMFk.jl
+++ src/NTFkNMFk.jl
@@ -1,29 +1,7 @@
 import NMFk
 import DocumentFunction
 
-function matrixfactorization(X::AbstractArray{T,N}, range::Union{AbstractRange{Int},Integer}, dims::Union{AbstractRange{Int},Integer}=1:N, aw...; casefilename="tensor-nmfk", kw...) where {T <: Number, N}
-	@assert maximum(dims) <= N
-	M = Vector{Tuple}(undef, N)
-	for d = dims
-		m = NMFk.flatten(X, d)
-		@info("Dimension $d in range $(range[d]) with size $(size(m)):")
-		M[d] = NMFk.execute(m, range[d], aw...; casefilename=casefilename * "_dim_$(d)", kw...)
-end
-	return M
-end
-
-function matrixfactorization(X::AbstractArray{T,N}, range::AbstractVector, aw...; casefilename="tensor-nmfk", kw...) where {T <: Number, N}
-	@assert length(range) == N
-	M = Vector{Tuple}(undef, N)
-	for d = 1:N
-		if length(range[d]) > 0
-			m = NMFk.flatten(X, d)
-			@info("Dimension $d in range $(range[d]) with size $(size(m)):")
-			M[d] = NMFk.execute(m, range[d], aw...; casefilename=casefilename * "_dim_$(d)", kw...)
-		end
-	end
-	return M
-end
+matrixfactorization = NMFk.tensorfactorization
 
 @doc """
 Matrix Factorization
